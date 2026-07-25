fd147a4ccf804313a95537dfe3d90041d1e0c81a
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-07-24 23:28:18 -0400

    fixed route, auth should be completed.

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -3,6 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 const CLIENT_ID = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID!;
 const REDIRECT_URI = "http://127.0.0.1:3000/api/auth/callback";
 const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+const APP_ORIGIN = "http://127.0.0.1:3000";
 
 export async function GET(request: NextRequest) {
   // Look at the url parameters and get the code, as well as the codeVerifier from the cookies.
@@ -50,7 +51,7 @@ export async function GET(request: NextRequest) {
 
   // Redirect user to Dashboard now that they are authetnicated and store token in cookie.
 
-  const res = NextResponse.redirect(new URL("/dashboard", request.url));
+  const res = NextResponse.redirect(new URL("/dashboard", APP_ORIGIN));
   res.cookies.set('access_token', data.access_token, {
     httpOnly: true,
     sameSite: 'lax',
