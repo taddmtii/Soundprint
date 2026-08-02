8680f979c301c48129257545f39fa3ee66b50f41
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-08-02 00:48:48 -0400

    updated user type

--- app/types/SpotifyUser.ts
+++ app/types/SpotifyUser.ts
@@ -1,12 +1,23 @@
 interface SpotifyUser {
   account_id: string;
+  country?: string;
   display_name: string | null;
-  external_urls?: {
+  email?: string;
+  explicit_content?: {
+    filter_enabled: boolean;
+    filter_locked: boolean;
+  };
+  external_urls: {
     spotify: string;
   };
-  href?: string;
-  id?: string;
-  images?: { url: string; height: number | null; width: number | null }[];
-  type?: string;
-  uri?: string;
+  followers: {
+    href: string | null;
+    total: number;
+  };
+  href: string;
+  id: string;
+  images: { url: string; height: number | null; width: number | null }[];
+  product?: string;
+  type: string;
+  uri: string;
 }
