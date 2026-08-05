631b41b3420fa2717e0a5e45c95e82b29396c0bb
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-08-05 18:08:14 -0400

    implemented basic laoding for toptracks

--- app/components/TopTracksCard.tsx
+++ app/components/TopTracksCard.tsx
@@ -1,11 +1,13 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { Music2 } from "lucide-react"
 
 interface TopTracksCardProps {
   content: SpotifyTrack[] | undefined,
+  isLoading: boolean;
 }
 
-export default function TopTracksCard({content}: TopTracksCardProps) {
+export default function TopTracksCard({content, isLoading}: TopTracksCardProps) {
     const top5 = content?.slice(0,5)
     return (
       <div className="w-125 h-100">
@@ -17,18 +19,35 @@ export default function TopTracksCard({content}: TopTracksCardProps) {
           </div>
         </CardHeader>
         <CardContent className="flex flex-col gap-2 text-lg font-bold">
-          {top5?.map((track, index) => (
-            <div key={track.id} className="flex items-center gap-3">
-              <span>{index + 1}</span>
-              {track.album.images[0].url && (
-                <img src={track.album.images[0].url} alt={track.name} className="h-12 w-12 rounded object-cover" />
-              )}
-              <div className="flex flex-col gap-1">
-                <div>{track.name}</div>
-                <div className="text-muted-foreground text-sm">{track.artists[0].name}</div>
+{         isLoading ? (
+            Array.from({ length: 5 }).map((_, index) => (
+              <div key={index} className="flex items-center gap-3">
+                <Skeleton className="h-4 w-4" />
+                <Skeleton className="h-12 w-12 rounded" />
+                <div className="flex flex-col gap-1">
+                  <Skeleton className="h-4 w-32" />
+                  <Skeleton className="h-3 w-20" />
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : (
+            top5?.map((track, index) => (
+              <div key={track.id} className="flex items-center gap-3">
+                <span>{index + 1}</span>
+                {track.album.images[0].url && (
+                  <img
+                    src={track.album.images[0].url}
+                    alt={track.name}
+                    className="h-12 w-12 rounded object-cover"
+                  />
+                )}
+                <div className="flex flex-col gap-1">
+                  <div>{track.name}</div>
+                  <div className="text-muted-foreground text-sm">{track.artists[0].name}</div>
+                </div>
+              </div>
+            ))
+          )}
         </CardContent>
       </Card>
       </div>
