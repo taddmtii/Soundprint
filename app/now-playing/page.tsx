cdaa651f1ab5d075b28a43dabd90d5bc5d0eef54
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-08-15 18:20:59 -0400

    updated background color

--- app/now-playing/page.tsx
+++ app/now-playing/page.tsx
@@ -9,12 +9,12 @@ export default function NowPlaying() {
   const data = useFetchData();
   return (
     <div className="flex flex-col gap-4 items-center">
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col">
         <h1 className="text-2xl font-bold">Now playing</h1>
         <span className="text-muted-foreground">What is coming out of your speakers right now. Whats next?</span>
       </div>
       <Separator />
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 items-center">
         <CurrentlyPlayingCard content={data.currentlyPlaying} isLoading={data.isLoading} />
         <Separator />
         <RecentlyPlayedCard content={data.recentlyPlayed?.items} isLoading={data.isLoading} />
