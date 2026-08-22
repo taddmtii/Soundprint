0afecdafc5bbee40645405c648cb16a046216bf6
Tadd Trumbull II <taddtrumbullii@gmail.com>
2026-08-21 21:18:50 -0400

    added top artists to each, top artists hook working as expected

--- app/12-months/page.tsx
+++ app/12-months/page.tsx
@@ -1,9 +1,17 @@
+'use client'
+
 import Header from "../components/Header";
+import TopArtists from "../components/TopArtists";
+import { useTopArtists } from "../hooks/useTopArtists";
 
 export default function TwelveMonthsPage() {
-  return (
-    <div className="w-full">
-      <Header />
-    </div>
-  )
+    const {data, isLoading} = useTopArtists("long_term");
+    return (
+      <div className="w-full">
+        <Header />
+        <div className="flex flex-col gap-2">
+          <TopArtists data={data} isLoading={isLoading}  />
+        </div>
+      </div>
+    )
 }
